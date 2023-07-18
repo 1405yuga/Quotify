c4be836ded127244edfcdc47edfb0a3b5663f183
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-18 15:48:28 +0530

    second UI test-intent implemented

--- app/src/androidTest/java/com/cheezycode/quotify/MainActivityTest.kt
+++ app/src/androidTest/java/com/cheezycode/quotify/MainActivityTest.kt
@@ -1,12 +1,17 @@
 package com.cheezycode.quotify
 
+import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.matcher.ViewMatchers.hasImeAction
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 
@@ -31,6 +36,15 @@ class MainActivityTest {
     fun testShareButton_expectedIntentchooser(){
         //arrange
         Intents.init()
+        val expected = allOf(hasAction(Intent.ACTION_SEND))
+
+        //act
+        onView(withId(R.id.floatingActionButton)).perform(click())
+
+        //assert
+        intended(expected)
+
+        Intents.release()
     }
 
 }
