221fae3bac3eba808740b8504254ca27c48864d0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-18 15:10:33 +0530

    created rule

--- app/src/androidTest/java/com/cheezycode/quotify/MainActivityTest.kt
+++ app/src/androidTest/java/com/cheezycode/quotify/MainActivityTest.kt
@@ -1,7 +1,18 @@
 package com.cheezycode.quotify
 
-import org.junit.Assert.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
 
-class MainActivityTest{
+class MainActivityTest {
+
+    @get:Rule //rule will be implemented before every test cases
+    val actvityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+
+    @Test
+    fun testNextButton() {
+
+    }
 
 }
