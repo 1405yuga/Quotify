7a42ef8f0d4d25158e47d641d91220370cd3ef0c
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-18 15:37:53 +0530

    intent espresso dependency added

--- app/src/androidTest/java/com/cheezycode/quotify/MainActivityTest.kt
+++ app/src/androidTest/java/com/cheezycode/quotify/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.cheezycode.quotify
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -24,6 +25,12 @@ class MainActivityTest {
 
         //assert
         onView(withId(R.id.quoteAuthor)).check(matches(withText("Johann Wolfgang von Goethe")))
+    }
+
+    @Test
+    fun testShareButton_expectedIntentchooser(){
+        //arrange
+        Intents.init()
     }
 
 }
